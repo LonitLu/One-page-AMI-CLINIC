3295da40142fc36492678c4781557154a9ea2b36
LonitLu <dimasik17021989@gmail.com>
2023-11-12 01:14:04 +0500

    "аудио отзыв" закончил блок

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ const swiper = new Swiper(".swiper", {
 
 
 document.getElementById( 'step__progressbar--line' ).value = 60;
+
+// при нажатии на кнопку=текст запускается воспроизведение аудио
+// условие, которое проверяет, играет ли аудио. Если аудио не играет, то запускается воспроизведение, иначе аудио ставится на паузу.
+
+const audioButton = document.getElementById("audioButton");
+const audioPlayer = document.getElementById("audioPlayer");
+
+audioButton.addEventListener("click", () => {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+  } else {
+    audioPlayer.pause();
+  }
+});
